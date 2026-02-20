e60103d9ec7ed1aad287de54adf724ab73971ddb
Priyanka-saw <priyankasaw51@gmail.com>
2026-02-20 23:26:03 +0530

    data  types and user inputs

---
+++ dataTypes_userInputs.py
@@ -0,0 +1,58 @@
+'''
+data types are the values that can be stored in the variables
+#1. string
+#2. integer 
+#3. float
+#4. boolean
+
+#1. string
+# it is a sequence of characters enclosed in single or double quotes    
+name = "priyanka"
+print(name)
+
+integer
+# it is a whole number without a decimal point
+age = 25
+print(age)
+
+#3. float
+# it is a number with a decimal point
+pi = 3.14
+print(pi)
+
+#4. boolean
+# it is a data type that can have only two values: True or False
+is_student = True
+print(is_student)
+
+'''
+
+#examples
+name = "Riya"
+print("Name:", name)
+
+age = 25
+print("Age:", age)
+
+pi = 3.14
+print("value of pi:", pi)
+
+is_student = True
+print("boolean value:", is_student)
+
+
+#user inputs
+# it is a way to get input from the user during the program execution   
+
+name = input("Enter your name: ")
+print("Hello", name)
+
+age = input("Enter your age: ")
+print("Your age is", age)
+
+age2 = int(input("enter your age: "))
+print("Your age is", age2)
+
+#evaluate is used to evaluate the expression and return the result
+age3 = eval(input("enter your age: "))
+print("Your age is", age3)
---
+++ var.py
@@ -0,0 +1,20 @@
+#variables are the polceholders for the values that can be used in the program
+
+# it is a container that holds data inside it as a value
+
+# for eg
+name = "priyanka"
+print(name)
+
+#rules
+#1. variable name should start with a letter or an underscore
+#2. variable name can only contain letters, numbers and underscores 
+#3. variable name cannot start with a number
+#4. variable name cannot be a reserved keyword
+# case-sensitive
+
+'''
+name1 = "priyanka"
+print(Name)  it shows error because of case-sensitive
+
+'''
