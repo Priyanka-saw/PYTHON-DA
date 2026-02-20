6c6d00ad4a7d761b1ebbf15dffcfadb75f67f648
Priyanka-saw <priyankasaw51@gmail.com>
2026-02-20 09:21:32 +0530

    first program

---
+++ firstPro.py
@@ -0,0 +1,11 @@
+#print("Priyanka")
+
+# used for the next line
+#print("""Priyanka
+ #     learning Python""")      
+
+#print("Priyanka\nlearning Python")
+
+# used for the tab space
+print("Priyanka\tlearning Python")
+
